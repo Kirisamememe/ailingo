22aa1eb9401898309bf822f5406252a6b523c4ab
Kirisame <wuyuhuahen@gmail.com>
2025-06-01 21:33:58 +0900

    feat: 生成AI用のクラスを実装

---
+++ src/lib/ai/ai.ts
@@ -0,0 +1,53 @@
+import "server-only";
+import { deepseek } from "@ai-sdk/deepseek";
+import { google } from "@ai-sdk/google";
+import { openai } from "@ai-sdk/openai";
+import type { LanguageModelV1 } from "ai";
+import { streamObject } from "ai";
+import type { z } from "zod";
+import {
+  type AIModel,
+  deepseekModelListTuple,
+  geminiModelListTuple,
+  openAIModelListTuple,
+} from "@/types";
+
+type Models = Record<AIModel, LanguageModelV1>;
+
+/**
+ * AI SDK
+ */
+class AI {
+  private geminiModels = Object.fromEntries(
+    geminiModelListTuple.map((model) => [model, google(model, { structuredOutputs: false })]),
+  ) as Record<(typeof geminiModelListTuple)[number], LanguageModelV1>;
+
+  private openAIModels = Object.fromEntries(
+    openAIModelListTuple.map((model) => [model, openai(model)]),
+  ) as Record<(typeof openAIModelListTuple)[number], LanguageModelV1>;
+
+  private deepseekModels = Object.fromEntries(
+    deepseekModelListTuple.map((model) => [model, deepseek(model)]),
+  ) as Record<(typeof deepseekModelListTuple)[number], LanguageModelV1>;
+
+  private models: Models = {
+    ...this.geminiModels,
+    ...this.openAIModels,
+    ...this.deepseekModels,
+  };
+
+  generate(model: AIModel, prompt: string, system: string, schema: z.ZodType) {
+    return streamObject({
+      model: this.models[model],
+      prompt: prompt,
+      system: system,
+      schema: schema,
+      maxTokens: 2000,
+    });
+  }
+}
+
+/**
+ * AI SDKのインスタンス
+ */
+export const ai = new AI();
