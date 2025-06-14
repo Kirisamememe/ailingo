b6cc59f18666af258256ae87d2bf35715379cf17
Kirisame <wuyuhuahen@gmail.com>
2025-06-14 20:20:22 +0900

    feat: exampleのスピーチ機能を実装

--- src/app/[locale]/(protected)/wordbook/_components/content/example/example.tsx
+++ src/app/[locale]/(protected)/wordbook/_components/content/example/example.tsx
@@ -1,5 +1,9 @@
+import { Volume2 } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import { FlexColumn } from "@/components/ui/flexbox";
 import { Caption, Paragraph } from "@/components/ui/typography";
+import { useSpeech } from "../../../_hooks/use-speech";
 
 type Props = {
   example: string;
@@ -10,15 +14,35 @@ type Props = {
  */
 export const Example: React.FC<Props> = ({ example }) => {
   const [sentence, translation] = example.split("\n");
+  const { play, stop, isPlaying } = useSpeech();
+
+  const handlePlay = async (sentence: string) => {
+    if (isPlaying) {
+      stop();
+    } else {
+      await play(sentence);
+    }
+  };
 
   return (
-    <FlexColumn gap={1} className="relative pl-3">
+    <FlexColumn className="relative pl-3">
       <Paragraph
-        size={16}
+        size={18}
         weight={500}
-        className="before:bg-primary before:absolute before:top-3 before:left-0 before:size-1 before:rounded-full before:content-['']"
+        className={cn(
+          "align-baseline",
+          "before:bg-primary before:absolute before:top-3 before:left-0 before:size-1 before:rounded-full before:content-['']",
+        )}
       >
         {sentence}
+        <Button
+          variant={isPlaying ? "coloredOutline" : "outline"}
+          size="icon"
+          className={cn("mb-0.5 ml-2 size-7 rounded-sm align-middle")}
+          onClick={() => handlePlay(sentence)}
+        >
+          <Volume2 className="size-4" />
+        </Button>
       </Paragraph>
       {translation && <Caption size={12}>{translation}</Caption>}
     </FlexColumn>
