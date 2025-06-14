fc6fc86d50c42e248e12c1a90a3157c8a08268dc
Kirisame <wuyuhuahen@gmail.com>
2025-06-14 19:02:28 +0900

    feat: azureTTSの関数を実装

---
+++ src/lib/azure-tts.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+
+type Result = {
+  data?: string;
+  error?: string;
+};
+
+/**
+ * 音声合成
+ * @param text テキスト
+ * @returns 音声合成結果
+ */
+export async function synthesizeSpeech(text: string) {
+  if (!process.env.SPEECH_KEY || !process.env.SPEECH_REGION) {
+    throw new Error("環境変数が設定されていません");
+  }
+
+  const speechConfig = sdk.SpeechConfig.fromSubscription(
+    process.env.SPEECH_KEY,
+    process.env.SPEECH_REGION,
+  );
+
+  // The language of the voice that speaks.
+  speechConfig.speechSynthesisVoiceName = "en-US-JennyNeural";
+
+  // Create the speech synthesizer.
+  const speechSynthesizer = new sdk.SpeechSynthesizer(speechConfig);
+
+  return new Promise<Result>((resolve) => {
+    speechSynthesizer.speakTextAsync(
+      text,
+      (result) => {
+        const { audioData } = result;
+
+        speechSynthesizer.close();
+        const base64Audio = Buffer.from(audioData).toString("base64");
+
+        resolve({
+          data: base64Audio,
+        });
+      },
+      (errorMsg) => {
+        speechSynthesizer.close();
+
+        resolve({
+          error: errorMsg,
+        });
+      },
+    );
+  });
+}
