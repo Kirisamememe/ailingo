46a519ac91d76b7530ea6a48ecd59cf35d5102fb
Kirisame <wuyuhuahen@gmail.com>
2025-06-15 11:52:40 +0900

    feat: wordcardフォームデータ生成用関数を実装

---
+++ src/app/[locale]/(protected)/wordbook/_utils/index.ts
@@ -0,0 +1,22 @@
+import type { WordCard } from "@/generated/prisma";
+
+/**
+ * 単語カードフォームデータを取得
+ * @param wordCard 単語カード
+ * @returns 単語カードフォームデータ
+ */
+export const getWordCardFormData = (wordCard?: WordCard) => {
+  return {
+    word: wordCard?.word ?? "",
+    phonetics: wordCard?.phonetics ?? "",
+    definitions: wordCard?.definitions ?? "",
+    example1: wordCard?.example1 ?? "",
+    example2: wordCard?.example2 ?? "",
+    example3: wordCard?.example3 ?? "",
+    derivatives: wordCard?.derivatives ?? "",
+    synonyms: wordCard?.synonyms ?? "",
+    antonyms: wordCard?.antonyms ?? "",
+    note: wordCard?.note ?? "",
+    language: wordCard?.language ?? "EN",
+  };
+};
