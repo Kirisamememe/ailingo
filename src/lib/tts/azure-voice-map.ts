b18d7eb83ae73b52e033630cb03ec5f3cd9ef468
Kirisame <wuyuhuahen@gmail.com>
2025-06-29 21:50:41 +0900

    fix: 音声マップを更新

--- src/lib/tts/azure-voice-map.ts
+++ src/lib/tts/azure-voice-map.ts
@@ -2,8 +2,14 @@
  * Azure Voice Map
  */
 export const azureVoiceMap = {
-  en: "en-US-JennyNeural",
+  "en-US": "en-US-JennyNeural",
+  "en-GB": "en-GB-SoniaNeural",
   ja: "ja-JP-NanamiNeural",
   "zh-CN": "zh-CN-XiaoxiaoNeural",
   "zh-TW": "zh-TW-HsiaoChenNeural",
+  "ko-KR": "ko-KR-SunHiNeural",
+  "de-DE": "de-DE-KatjaNeural",
+  "es-ES": "es-ES-LauraNeural",
+  "fr-FR": "fr-FR-VivienneNeural",
+  "it-IT": "it-IT-ElsaNeural",
 };
