74efc163b81e1980e185be0fb1c81f0005d60002
hanjunyi <wuyuhuahen@gmail.com>
2025-07-06 21:15:07 +0900

    fix: modify migration directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "postgresql",
-  out: "./src/drizzle",
+  out: "./src/drizzle/migrations",
   schema: "./src/drizzle/schema",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "",
---
+++ src/drizzle/migrations/0005_rename_composition_sequence.sql
@@ -0,0 +1,2 @@
+-- Custom SQL migration file, put your code below! --
+ALTER SEQUENCE composition_id_seq RENAME TO writing_id_seq;
