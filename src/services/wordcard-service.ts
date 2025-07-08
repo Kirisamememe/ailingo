60cc33074decf5d983023d4e3dbf3b163a74fc7a
hanjunyi <wuyuhuahen@gmail.com>
2025-07-09 03:11:11 +0900

    feat: add methods to fetch daily new and review words

--- src/services/wordcard-service.ts
+++ src/services/wordcard-service.ts
@@ -1,5 +1,5 @@
 import "server-only";
-import { and, desc, eq, isNull } from "drizzle-orm";
+import { and, asc, desc, eq, isNull, lte } from "drizzle-orm";
 import type z from "zod";
 import { db, dbExceptionHandler } from "@/lib/db";
 import type { wordcardFormSchema } from "@/app/[locale]/(protected)/wordbook/_schema/wordcard";
@@ -80,6 +80,52 @@ class WordCardService {
       .set({ deletedAt: new Date() })
       .where(eq(wordCard.id, id))
       .catch(dbExceptionHandler);
+  }
+
+  /**
+   * 今日覚える新しい単語を取得する
+   */
+  async getDailyNewWords(userId: string, number: number) {
+    const result = await db
+      .select({ id: wordCard.id, word: wordCard.word })
+      .from(wordCard)
+      .where(
+        and(
+          eq(wordCard.authorId, userId),
+          eq(wordCard.retentionRate, 1),
+          isNull(wordCard.deletedAt),
+          isNull(wordCard.masteredAt),
+        ),
+      )
+      .orderBy(
+        asc(wordCard.createdAt),
+        asc(wordCard.id),
+        asc(wordCard.updatedAt),
+        asc(wordCard.word),
+      )
+      .limit(number)
+      .catch(dbExceptionHandler);
+    return result;
+  }
+
+  /**
+   * 今日復習する単語を取得する
+   */
+  async getDailyReviewWords(userId: string, date: string) {
+    const result = await db
+      .select({ id: wordCard.id, word: wordCard.word })
+      .from(wordCard)
+      .where(
+        and(
+          eq(wordCard.authorId, userId),
+          isNull(wordCard.deletedAt),
+          isNull(wordCard.masteredAt),
+          lte(wordCard.nextReviewAt, date),
+        ),
+      )
+      .orderBy(asc(wordCard.lastReviewedAt))
+      .catch(dbExceptionHandler);
+    return result;
   }
 }
 
