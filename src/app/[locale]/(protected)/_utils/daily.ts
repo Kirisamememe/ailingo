e14e9d1f97f268f7fcf76c3f42b4e4dd0a27846a
hanjunyi <wuyuhuahen@gmail.com>
2025-07-12 02:19:02 +0900

    feat: add daily learning plan utility functions

---
+++ src/app/[locale]/(protected)/_utils/daily.ts
@@ -0,0 +1,31 @@
+import { getCurrentDate } from "@/lib/utils";
+import { wordCardService } from "@/services";
+import { dailyService } from "@/services/daily-service";
+import "server-only";
+
+/**
+ * 日々の学習計画を作成する
+ * @param userId ユーザーID
+ */
+export const planDailyLearning = async (userId: string) => {
+  const date = getCurrentDate();
+  const lastLearning = await dailyService.getLastLearning(userId);
+
+  if (!lastLearning || lastLearning.date !== date) {
+    const newWords = await wordCardService.getDailyNewWords(userId, 10);
+    const reviewWords = await wordCardService.getDailyReviewWords(userId, date);
+
+    const newWordsString = newWords.map((word) => `${word.id}|${word.word}`).join(",");
+    const reviewWordsString = reviewWords.map((word) => `${word.id}|${word.word}`).join(",");
+
+    if (
+      newWordsString === lastLearning?.newWords &&
+      reviewWordsString === lastLearning.reviewWords
+    ) {
+      await dailyService.updateDailyLearning(userId, lastLearning.id, date);
+      return;
+    }
+
+    await dailyService.createDailyLearning(userId, date, newWordsString, reviewWordsString);
+  }
+};
