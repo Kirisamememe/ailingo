db1982e4ccdb0a575dbb1a85968a6a6500be0681
hanjunyi <wuyuhuahen@gmail.com>
2025-07-18 21:14:07 +0900

    feat: update wordCard list

--- src/app/[locale]/(protected)/wordbook/_components/list/list.tsx
+++ src/app/[locale]/(protected)/wordbook/_components/list/list.tsx
@@ -1,39 +1,43 @@
 "use client";
 
-import { usePathname, useSearchParams } from "next/navigation";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { useEffect } from "react";
+import { FlexColumn } from "@/components/ui/flexbox";
 import { ListItem } from "./list-item";
 import { WordbookListNew } from "./list-new";
 import { WordbookListStreaming } from "./list-streaming";
-import type { WordListItem } from "@/types";
-
-type Props = {
-  wordList: WordListItem[];
-};
+import { useWordbookStore } from "../../_hooks/store-provider";
+import { convertWordCardData } from "../../_utils";
 
 /**
  * ワードブックリスト
  */
-export const WordbookList: React.FC<Props> = ({ wordList }) => {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const wordCardId = Number(searchParams.get("wordCardId"));
+export const WordbookList = () => {
+  const wordCards = useWordbookStore((state) => state.wordCards);
+  const nextWord = useWordbookStore((state) => state.nextWord);
+  const prevWord = useWordbookStore((state) => state.prevWord);
 
-  const onClick = (id: number) => {
-    const currentSearchParams = new URLSearchParams(searchParams);
-    currentSearchParams.set("wordCardId", id.toString());
+  useEffect(() => {
+    const onFocusChange = (e: KeyboardEvent) => {
+      if (e.key === "ArrowDown" || (e.key === "Tab" && !e.shiftKey)) {
+        nextWord(e);
+      } else if (e.key === "ArrowUp" || (e.key === "Tab" && e.shiftKey)) {
+        prevWord(e);
+      }
+    };
 
-    const newUrl = `${pathname}?${currentSearchParams.toString()}`;
-    window.history.replaceState(null, "", newUrl);
-  };
+    window.addEventListener("keydown", onFocusChange);
+    return () => {
+      window.removeEventListener("keydown", onFocusChange);
+    };
+  }, [prevWord, nextWord]);
 
   return (
-    <ScrollArea className="w-20 shrink-0 @[36rem]:w-48 @[40rem]:w-64 [&_[data-slot='scroll-area-scrollbar']]:pt-16">
-      <WordbookListNew selectedId={wordCardId} onClick={onClick} />
+    <FlexColumn className="w-full">
+      <WordbookListNew />
       <WordbookListStreaming />
-      {wordList.map((word) => (
-        <ListItem key={word.id} listItem={word} selectedId={wordCardId} onClick={onClick} />
+      {wordCards.map((wordCard, index) => (
+        <ListItem key={wordCard.id} index={index} listItem={convertWordCardData(wordCard)} />
       ))}
-    </ScrollArea>
+    </FlexColumn>
   );
 };
