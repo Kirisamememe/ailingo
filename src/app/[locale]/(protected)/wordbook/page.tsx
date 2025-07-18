74766a2feb3ee9a6e0bdfe771f52bfa2a7572a3f
hanjunyi <wuyuhuahen@gmail.com>
2025-07-18 21:09:24 +0900

    refactor: apply new states

--- src/app/[locale]/(protected)/wordbook/page.tsx
+++ src/app/[locale]/(protected)/wordbook/page.tsx
@@ -4,42 +4,30 @@ import { getSession } from "@/lib/auth";
 import { InsetLayoutWithPadding } from "@/components/layout";
 import { WordbookContent } from "./_components/content";
 import { WordbookList } from "./_components/list";
+import { GenerateFormProvider } from "./_hooks/generate-form-provider";
 import { StoreProvider } from "./_hooks/store-provider";
-import { WordbookProvider } from "./_hooks/wordbook-provider";
-import { convertWordCardData } from "./_utils";
 import { getCookie } from "../_actions/cookies";
 import { wordCardService } from "@/services";
 import type { LanguageCode } from "@/types";
 
 const WordbookPage = async () => {
   const { operatorId } = await getSession();
-  const wordCards = await wordCardService.getMany(operatorId);
-  const wordCardsClient = wordCards.map(convertWordCardData);
-  const wordList = wordCardsClient.map((wordCard) => {
-    const { id, entry, language, phonetics, definitions, examples } = wordCard;
-    return {
-      id,
-      entry,
-      language,
-      phonetics,
-      definitions,
-      examples,
-    };
-  });
 
   const modelCookie = await getCookie("WORDCARD_MODEL");
   const model = (modelCookie ?? modelListTuple[5]) as AIModel;
   const translationLanguageCookie = await getCookie("WORDCARD_TRANSLATION_LANGUAGE");
   const translationLanguage = (translationLanguageCookie ?? "en-US") as LanguageCode;
 
+  const wordCards = wordCardService.getMany(operatorId);
+
   return (
-    <StoreProvider>
-      <WordbookProvider model={model} translationLanguage={translationLanguage}>
-        <InsetLayoutWithPadding className="w-full max-w-360 flex-row gap-0 @[36rem]:gap-4">
-          <WordbookList wordList={wordList} />
-          <WordbookContent wordCards={wordCards} />
+    <StoreProvider wordCards={wordCards}>
+      <GenerateFormProvider model={model} translationLanguage={translationLanguage}>
+        <InsetLayoutWithPadding className="w-full max-w-360 flex-row gap-0 sm:gap-4">
+          <WordbookList />
+          <WordbookContent />
         </InsetLayoutWithPadding>
-      </WordbookProvider>
+      </GenerateFormProvider>
     </StoreProvider>
   );
 };
