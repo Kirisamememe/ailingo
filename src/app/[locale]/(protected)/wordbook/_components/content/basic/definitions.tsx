92b6fabd8823cbf3d1a7e9ae7611d194223b0b7c
hanjunyi <wuyuhuahen@gmail.com>
2025-07-18 13:32:05 +0900

    fix: delete unnecessary if condition

--- src/app/[locale]/(protected)/wordbook/_components/content/basic/definitions.tsx
+++ src/app/[locale]/(protected)/wordbook/_components/content/basic/definitions.tsx
@@ -2,7 +2,6 @@ import { useTranslations } from "next-intl";
 import { Badge } from "@/components/ui/badge";
 import { FlexColumn } from "@/components/ui/flexbox";
 import { Caption } from "@/components/ui/typography";
-import { definitionsArraySchema } from "../../../_schema";
 import type { WordCardClient } from "@/types";
 
 type Props = Pick<WordCardClient, "definitions" | "language">;
@@ -12,12 +11,6 @@ type Props = Pick<WordCardClient, "definitions" | "language">;
  */
 export const Definitions: React.FC<Props> = ({ definitions, language }) => {
   const t = useTranslations("POS");
-
-  const parsedDefinitionsArray = definitionsArraySchema.safeParse(definitions);
-
-  if (!parsedDefinitionsArray.success) {
-    return <Caption>{definitions.map((definition) => definition.meaning).join("\n")}</Caption>;
-  }
 
   return (
     <FlexColumn gap={3}>
