eaa56065712b00f3cd672033933fbcd39fbf1935
hanjunyi <wuyuhuahen@gmail.com>
2025-07-18 21:13:49 +0900

    feat: update wordCard list item

--- src/app/[locale]/(protected)/wordbook/_components/list/list-item.tsx
+++ src/app/[locale]/(protected)/wordbook/_components/list/list-item.tsx
@@ -1,26 +1,27 @@
 "use client";
 
+import { useRef } from "react";
 import { ListItemView } from "./list-item-view";
+import { useWordbookStore } from "../../_hooks/store-provider";
 import type { WordListItem } from "@/types";
 
 type Props = {
+  index: number;
   listItem: WordListItem;
-  selectedId: number;
-  onClick: (id: number) => void;
 };
 
 /**
  * ワードブックリストアイテム
  */
-export const ListItem: React.FC<Props> = ({ listItem, selectedId, onClick }) => {
-  return (
-    <ListItemView
-      word={listItem.word}
-      language={listItem.language}
-      isSelected={selectedId === listItem.id}
-      onClick={() => {
-        onClick(listItem.id);
-      }}
-    />
-  );
+export const ListItem: React.FC<Props> = ({ index, listItem }) => {
+  const ref = useRef<HTMLButtonElement>(null);
+  const setSelectedWordCardIndex = useWordbookStore((state) => state.setSelectedWordCardIndex);
+
+  const onClick = () => {
+    if (!ref.current) return;
+
+    setSelectedWordCardIndex(index, ref.current);
+  };
+
+  return <ListItemView ref={ref} listItem={listItem} onClick={onClick} />;
 };
