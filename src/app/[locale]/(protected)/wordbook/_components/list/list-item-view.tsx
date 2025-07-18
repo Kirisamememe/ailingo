3ceb6d2e10aaf94ef564a76c90cbbff585b9d5a3
hanjunyi <wuyuhuahen@gmail.com>
2025-07-18 21:13:08 +0900

    feat: update wordCard item styles

--- src/app/[locale]/(protected)/wordbook/_components/list/list-item-view.tsx
+++ src/app/[locale]/(protected)/wordbook/_components/list/list-item-view.tsx
@@ -1,35 +1,83 @@
+import type { ComponentPropsWithRef } from "react";
+import { type ComponentProps, Fragment } from "react";
+import { useTranslations } from "next-intl";
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { FlexColumn, Flexbox } from "@/components/ui/flexbox";
+import { Caption, Headline, Paragraph } from "@/components/ui/typography";
+import type { ListItem } from "./list-item";
 
 type Props = {
-  word: string;
-  language: string;
-  isSelected: boolean;
   onClick: () => void;
-};
+} & Pick<ComponentProps<typeof ListItem>, "listItem"> &
+  ComponentPropsWithRef<"button">;
 
 /**
  * ワードブックリストアイテムビュー
  */
-export const ListItemView: React.FC<Props> = ({ word, language, isSelected, onClick }) => {
+export const ListItemView: React.FC<Props> = ({ ref, listItem, onClick }) => {
+  const t = useTranslations("POS");
+
+  const { entry: word, language, phonetics, definitions, examples } = listItem;
+
   return (
-    <Button
+    <button
+      ref={ref}
+      type="button"
       lang={language}
       onClick={onClick}
-      variant="ghost"
       className={cn(
-        "relative h-10 w-full justify-start rounded-none px-1.5 text-[0.625rem] font-semibold",
-        "@[36rem]:h-13 @[36rem]:rounded-sm @[36rem]:px-4 @[36rem]:text-sm",
+        "relative flex h-fit w-full cursor-pointer flex-col items-start gap-4 rounded-sm px-4 py-3 text-left font-semibold sm:flex-row sm:justify-start",
         "dark:hover:bg-accent",
-        "active:scale-98",
         "after:bg-accent after:absolute after:-bottom-0.25 after:h-0.25 after:content-[''] last:after:hidden",
-        "after:left-0 after:w-full @[36rem]:after:left-3 @[36rem]:after:w-[calc(100%-1.5rem)]",
+        "after:left-0 after:w-full sm:after:left-3 sm:after:w-[calc(100%-1.5rem)]",
         "hover:after:bg-transparent",
-        isSelected &&
-          "bg-primary/5 border-primary hover:bg-primary/10 dark:hover:bg-primary/20 text-primary hover:text-primary border-1 after:bg-transparent @[36rem]:text-lg",
+        "data-[selected=true]:focus:outline-none",
+        "data-[selected=true]:focus-visible:outline-none",
+        "data-[selected=true]:hover:bg-primary/5 data-[selected=true]:bg-primary/5 dark:data-[selected=true]:hover:bg-primary/5 data-[selected=true]:shadow-[inset_0_0_0_2px_var(--primary)] data-[selected=true]:after:hidden",
+        "focus:after:hidden focus-visible:after:hidden",
       )}
     >
-      {word}
-    </Button>
+      <Flexbox className="shr ink-0 w-full flex-row items-center gap-2 whitespace-break-spaces sm:w-40 sm:flex-col sm:items-start sm:gap-0">
+        <Headline lang={language} height={1.5} className="h-6">
+          {word}
+        </Headline>
+        <Caption lang={language} className="" height={1.5}>
+          {phonetics}
+        </Caption>
+      </Flexbox>
+      <FlexColumn className="">
+        <Paragraph lang={language} clamp={1} className="pt-1 leading-none">
+          {definitions.map((definition) => (
+            <Fragment key={`${listItem.id}-${definition.meaning}`}>
+              <Badge
+                variant={"secondary"}
+                className="bg-foreground/15 mr-1 rounded-[0.25rem] px-1 py-0.5 text-xs leading-none font-bold"
+              >
+                {t(definition.pos)}
+              </Badge>
+              <Caption className="mr-4 text-sm" color="foreground">
+                {definition.translation ?? definition.meaning}
+              </Caption>
+            </Fragment>
+          ))}
+        </Paragraph>
+        <Paragraph lang={language} color="muted" clamp={1}>
+          {examples.map((example) => (
+            <Fragment key={`${listItem.id}-${example.sentence}`}>
+              <Caption
+                lang={language}
+                color="muted"
+                className={cn(
+                  "after:bg-foreground/15 relative mr-4 ml-2.5 after:absolute after:top-1/2 after:-left-2.5 after:h-2.5 after:w-1 after:-translate-y-1/2 after:rounded-xs after:content-['']",
+                )}
+              >
+                {example.sentence}
+              </Caption>
+            </Fragment>
+          ))}
+        </Paragraph>
+      </FlexColumn>
+    </button>
   );
 };
