a85530dc69fda7fd02e993a32203da87b0029b21
hanjunyi <wuyuhuahen@gmail.com>
2025-07-19 00:45:05 +0900

    refactor: update list item styles

--- src/app/[locale]/(protected)/wordbook/_components/list/list-item-view.tsx
+++ src/app/[locale]/(protected)/wordbook/_components/list/list-item-view.tsx
@@ -28,25 +28,24 @@ export const ListItemView: React.FC<Props> = ({ ref, listItem, onClick }) => {
       onClick={onClick}
       className={cn(
         "relative flex h-fit w-full cursor-pointer flex-col items-start gap-4 rounded-sm px-4 py-3 text-left font-semibold sm:flex-row sm:justify-start",
-        "dark:hover:bg-accent",
+        "hover:bg-accent",
         "after:bg-accent after:absolute after:-bottom-0.25 after:h-0.25 after:content-[''] last:after:hidden",
         "after:left-0 after:w-full sm:after:left-3 sm:after:w-[calc(100%-1.5rem)]",
         "hover:after:bg-transparent",
-        "data-[selected=true]:focus:outline-none",
-        "data-[selected=true]:focus-visible:outline-none",
-        "data-[selected=true]:hover:bg-primary/5 data-[selected=true]:bg-primary/5 dark:data-[selected=true]:hover:bg-primary/5 data-[selected=true]:shadow-[inset_0_0_0_2px_var(--primary)] data-[selected=true]:after:hidden",
+        "focus:shadow-primary/40 focus:shadow-[inset_0_0_0_2px] focus:outline-none focus-visible:outline-none",
+        "data-[selected=true]:hover:bg-primary/5 data-[selected=true]:bg-primary/5 dark:data-[selected=true]:hover:bg-primary/5 data-[selected=true]:shadow-primary data-[selected=true]:shadow-[inset_0_0_0_2px] data-[selected=true]:after:hidden",
         "focus:after:hidden focus-visible:after:hidden",
       )}
     >
-      <Flexbox className="shr ink-0 w-full flex-row items-center gap-2 whitespace-break-spaces sm:w-40 sm:flex-col sm:items-start sm:gap-0">
-        <Headline lang={language} height={1.5} className="h-6">
+      <Flexbox className="w-full shrink-0 flex-row items-center gap-2 whitespace-break-spaces sm:w-48 sm:flex-col sm:items-start sm:gap-1">
+        <Headline lang={language} height={1.5} className="min-h-6">
           {entry}
         </Headline>
         <Caption lang={language} className="" height={1.5}>
           {phonetics}
         </Caption>
       </Flexbox>
-      <FlexColumn className="">
+      <FlexColumn gap={1}>
         <Paragraph lang={language} clamp={1} className="pt-1 leading-none">
           {definitions.map((definition) => (
             <Fragment key={`${listItem.id}-${definition.meaning}`}>
@@ -69,7 +68,7 @@ export const ListItemView: React.FC<Props> = ({ ref, listItem, onClick }) => {
                 lang={language}
                 color="muted"
                 className={cn(
-                  "after:bg-foreground/15 relative mr-4 ml-2.5 after:absolute after:top-1/2 after:-left-2.5 after:h-2.5 after:w-1 after:-translate-y-1/2 after:rounded-xs after:content-['']",
+                  "after:bg-foreground/15 relative mr-4 ml-2.5 after:absolute after:top-0.5 after:-left-2.5 after:h-2.5 after:w-1 after:rounded-xs after:content-['']",
                 )}
               >
                 {example.sentence}
