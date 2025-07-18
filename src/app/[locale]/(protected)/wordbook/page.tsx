aac042e69926d88c7e35c67a758401e12b6a138b
hanjunyi <wuyuhuahen@gmail.com>
2025-07-18 13:13:49 +0900

    feat: apply the zustand store to the page component

--- src/app/[locale]/(protected)/wordbook/page.tsx
+++ src/app/[locale]/(protected)/wordbook/page.tsx
@@ -2,10 +2,9 @@ import type { AIModel } from "@/lib/ai";
 import { modelListTuple } from "@/lib/ai";
 import { getSession } from "@/lib/auth";
 import { InsetLayoutWithPadding } from "@/components/layout";
-import { FlexRow } from "@/components/ui/flexbox";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { WordbookContent } from "./_components/content";
 import { WordbookList } from "./_components/list";
+import { StoreProvider } from "./_hooks/store-provider";
 import { WordbookProvider } from "./_hooks/wordbook-provider";
 import { convertWordCardData } from "./_utils";
 import { getCookie } from "../_actions/cookies";
@@ -34,16 +33,14 @@ const WordbookPage = async () => {
   const translationLanguage = (translationLanguageCookie ?? "en-US") as LanguageCode;
 
   return (
-    <WordbookProvider model={model} translationLanguage={translationLanguage}>
-      <InsetLayoutWithPadding>
-        <FlexRow className="@[36rem]:bg-card/50 max-h-[calc(100dvh-3.5rem)] w-full flex-row-reverse gap-0 border-t sm:flex-row @[36rem]:gap-4 @[36rem]:rounded-lg @[36rem]:border @[36rem]:p-4 @[40rem]:max-h-[calc(100dvh-4.5rem)]">
+    <StoreProvider>
+      <WordbookProvider model={model} translationLanguage={translationLanguage}>
+        <InsetLayoutWithPadding className="w-full max-w-360 flex-row gap-0 @[36rem]:gap-4">
           <WordbookList wordList={wordList} />
-          <ScrollArea className="@[36rem]:bg-card/50 relative h-full w-full border-r p-3 @[36rem]:rounded-md @[36rem]:border @[36rem]:p-6 [&_[data-slot='scroll-area-scrollbar']]:mr-1 [&_[data-slot='scroll-area-scrollbar']]:py-2 [&>div>div]:h-full">
-            <WordbookContent wordCards={wordCards} />
-          </ScrollArea>
-        </FlexRow>
-      </InsetLayoutWithPadding>
-    </WordbookProvider>
+          <WordbookContent wordCards={wordCards} />
+        </InsetLayoutWithPadding>
+      </WordbookProvider>
+    </StoreProvider>
   );
 };
 
