8d88716321395c3fe03b64ab4c41869cc42bd04f
hanjunyi <wuyuhuahen@gmail.com>
2025-07-19 00:53:47 +0900

    feat: create HiddenWhenMobile component

---
+++ src/components/hidden-when-mobile.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import type { ReactNode } from "react";
+import { useIsMobile } from "@/hooks/use-mobile";
+
+type Props = {
+  children: ReactNode;
+};
+
+/**
+ * モバイルのみ非表示
+ */
+export const HiddenWhenMobile: React.FC<Props> = ({ children }) => {
+  const isMobile = useIsMobile();
+
+  return isMobile ? null : children;
+};
