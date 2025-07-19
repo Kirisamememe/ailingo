78428ba4d59eccf1d4ecd957b92097192c65fb43
hanjunyi <wuyuhuahen@gmail.com>
2025-07-20 01:18:14 +0900

    refactor: add util functions

--- src/app/[locale]/(protected)/wordbook/_utils/index.ts
+++ src/app/[locale]/(protected)/wordbook/_utils/index.ts
@@ -1,20 +1,20 @@
-import { mergeDefinitions, mergeExamples } from "@/lib/utils";
+import { mergeDefinitions } from "@/lib/utils";
 import { DB_DEFINITION_DIVIDER } from "@/constants";
-import type { POS, WordCard, WordCardClient } from "@/types";
+import type { POS, WordCard, WordCardListItem } from "@/types";
 
 /**
  * 単語カードフォームデータを取得
  * @param wordCard 単語カード
  * @returns 単語カードフォームデータ
  */
-export const getWordCardFormData = (wordCard?: WordCardClient) => {
+export const getWordCardFormData = (wordCard?: WordCard) => {
   return {
     entry: wordCard?.entry ?? "",
     phonetics: wordCard?.phonetics ?? "",
-    definitions: wordCard ? mergeDefinitions(wordCard.definitions) : "",
-    example1: wordCard ? mergeExamples(wordCard.examples[0]) : "",
-    example2: wordCard ? mergeExamples(wordCard.examples[1]) : "",
-    example3: wordCard ? mergeExamples(wordCard.examples[2]) : "",
+    definitions: wordCard?.definitions ?? "",
+    example1: wordCard?.example1 ?? "",
+    example2: wordCard?.example2 ?? "",
+    example3: wordCard?.example3 ?? "",
     collocations: wordCard?.collocations ?? "",
     derivatives: wordCard?.derivatives ?? "",
     synonyms: wordCard?.synonyms ?? "",
@@ -30,10 +30,64 @@ export const getWordCardFormData = (wordCard?: WordCardClient) => {
  * @param wordCards 単語カード
  * @returns クライアント用の単語カードデータ
  */
-export const convertWordCardDBToClient = (wordCard: WordCard): WordCardClient => {
-  const { definitions, example1, example2, example3, ...rest } = wordCard;
+export const convertWordCardDBToListItem = (wordCard: WordCard): WordCardListItem => {
+  const { definitions, ...rest } = wordCard;
 
   const definitionsArray = definitions
+    .split("\n")
+    .filter((definition) => !!definition)
+    .map((definition) => {
+      const parts = definition.split(DB_DEFINITION_DIVIDER);
+      const [posString, meaning, translation = undefined] = parts;
+
+      const pos = posString.replace(/^\[|\]$/g, "");
+
+      return {
+        pos: pos as POS,
+        meaning,
+        translation,
+      };
+    });
+
+  return {
+    ...rest,
+    definitions: definitionsArray,
+  };
+};
+
+/**
+ * 単語カードデータをデータベース用に変換
+ * @param wordCard 単語カード
+ * @returns データベース用の単語カードデータ
+ */
+export const convertWordCardClientToDB = (wordCard: WordCardListItem): WordCard => {
+  return {
+    ...wordCard,
+    definitions: mergeDefinitions(wordCard.definitions),
+  };
+};
+
+/**
+ * 例文を分割
+ * @param example 例文
+ * @returns 分割された例文
+ */
+export const splitExample = (example: string) => {
+  const parts = example.split("\n");
+  const [sentence, translation] = parts;
+  return {
+    sentence,
+    ...(translation && { translation }),
+  };
+};
+
+/**
+ * 定義を分割
+ * @param definitions 定義
+ * @returns 分割された定義
+ */
+export const splitDefinitions = (definitions: string) => {
+  return definitions
     .split("\n")
     .filter((definition) => !!definition)
     .map((definition) => {
@@ -50,36 +104,4 @@ export const convertWordCardDBToClient = (wordCard: WordCard): WordCardClient =>
       };
     })
     .filter((definition) => definition !== undefined);
-
-  const exampleArray = [example1, example2, example3]
-    .filter((example) => example !== null)
-    .map((example) => {
-      const parts = example.split("\n");
-      const [sentence, translation] = parts;
-      return {
-        sentence,
-        ...(translation && { translation }),
-      };
-    });
-
-  return {
-    ...rest,
-    definitions: definitionsArray,
-    examples: exampleArray,
-  };
-};
-
-/**
- * 単語カードデータをデータベース用に変換
- * @param wordCard 単語カード
- * @returns データベース用の単語カードデータ
- */
-export const convertWordCardClientToDB = (wordCard: WordCardClient): WordCard => {
-  return {
-    ...wordCard,
-    definitions: mergeDefinitions(wordCard.definitions),
-    example1: mergeExamples(wordCard.examples[0]),
-    example2: wordCard.examples[1] ? mergeExamples(wordCard.examples[1]) : null,
-    example3: wordCard.examples[2] ? mergeExamples(wordCard.examples[2]) : null,
-  };
 };
