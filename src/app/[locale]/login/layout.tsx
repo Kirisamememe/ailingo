925b033510dbe1a812668ba6bccaedda96644478
hanjunyi <wuyuhuahen@gmail.com>
2025-07-20 02:08:13 +0900

    feat: update layout

--- src/app/[locale]/login/layout.tsx
+++ src/app/[locale]/login/layout.tsx
@@ -5,7 +5,11 @@ type Props = {
 };
 
 const LoginLayout: React.FC<Props> = ({ children }) => {
-  return <BaseLayout>{children}</BaseLayout>;
+  return (
+    <BaseLayout>
+      <main className="grid min-h-dvh place-content-center">{children}</main>
+    </BaseLayout>
+  );
 };
 
 export default LoginLayout;
